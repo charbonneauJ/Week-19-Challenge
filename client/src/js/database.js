1bfc1e45218df11a29a5fed79344c6b3c534867e
Charbonneau John <john.charbonneau93@gmail.com>
2024-09-18 22:45:16 -0600

    Adding in code for database.js, did both todos to add in content and logic.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,70 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error("putDb not implemented");
 
+  const db = await openDB("jate", 1);
+
+  const tx = contactDb.transaction("jate", "readwrite");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.add(content);
+
+  const result = await request;
+
+  console.log("result", result);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error("getDb not implemented");
+
+  const contactDb = await openDB("jate", 1);
+
+  const tx = contactDb.transaction("jate", "readonly");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log("result.value", result);
+
+  return result;
+};
+
+// Making the function to delete the DB.
+
+export const deleteDb = async (id) => {
+  console.log("Delete from the DB", id);
+
+  const contactDb = await openDB("jate", 1);
+
+  const tx = contactDb.transaction("jate", "readwrite");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.delete(id);
+
+  const result = await request;
+
+  console.log("result.value", result);
+
+  return result?.value;
+};
 
 initdb();
